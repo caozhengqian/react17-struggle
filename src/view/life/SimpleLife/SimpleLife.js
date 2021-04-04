a13817bb3474373d2a95f2b10dde478c3c65e13f
1264419544@qq.com <1264419544@qq.com>
2021-04-04 23:12:43 +0800

    add simplelife

--- src/view/life/SimpleLife/SimpleLife.js
+++ src/view/life/SimpleLife/SimpleLife.js
@@ -3,12 +3,29 @@ import "./index.less";
 
 export default class SimpleLife extends React.Component {
   constructor(props) {
+    console.log(`%c Father-->1、constructor`, "color:#FF9797");
     super(props);
     this.state = {};
   }
 
+  static getDerivedStateFromProps(props, state){
+    console.log(`%c Father-->1、getDerivedStateFromProps`, "color:#FF9797");
+    return null;
+  }
   componentDidMount() {
-
+    console.log(`%c Father-->1、componentDidMount`, "color:#FF9797");
+  }
+  shouldComponentUpdate(){
+    console.log(`%c Father-->1、shouldComponentUpdate`, "color:#FF9797");
+  }
+  getSnapshotBeforeUpdate(){
+    console.log(`%c Father-->1、getSnapshotBeforeUpdate`, "color:#FF9797");
+  }
+  componentDidUpdate(){
+    console.log(`%c Father-->1、componentDidUpdate`, "color:#FF9797");
+  }
+  componentWillUnmount(){
+    console.log(`%c Father-->1、componentWillUnmount`, "color:#FF9797");
   }
 
   render() {
@@ -18,4 +35,5 @@ export default class SimpleLife extends React.Component {
         </div>
     );
   }
+
 }
