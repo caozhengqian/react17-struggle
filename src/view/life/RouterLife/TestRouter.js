45e2014635f40f5f851a490070ab309d6c87d24c
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-05-10 19:18:26 +0800

    add 路由跳转结束

--- src/view/life/RouterLife/TestRouter.js
+++ src/view/life/RouterLife/TestRouter.js
@@ -3,12 +3,33 @@ import "./index.less";
 
 export default class TestRouter extends React.Component {
   constructor(props) {
+    console.log(`%c TestRouter-->1、constructor`, "color:#FF9797");
     super(props);
     this.state = {};
   }
-
+  static getDerivedStateFromProps(props, state){
+    console.log(`%c TestRouter-->1、getDerivedStateFromProps`, "color:#FF9797");
+    return null;
+  }
   componentDidMount() {
-
+    console.log(`%c TestRouter-->1、componentDidMount`, "color:#FF9797");
+  }
+  shouldComponentUpdate(){
+    console.log(`%c TestRouter-->1、shouldComponentUpdate`, "color:#FF9797");
+    return true
+  }
+  getSnapshotBeforeUpdate(){
+    console.log(`%c TestRouter-->1、getSnapshotBeforeUpdate`, "color:#FF9797");
+    return null;
+  }
+  componentDidUpdate(){
+    console.log(`%c TestRouter-->1、componentDidUpdate`, "color:#FF9797");
+  }
+  componentWillUnmount(){
+    console.log(`%c TestRouter-->1、componentWillUnmount`, "color:#FF9797");
+  }
+  handleClick(){
+    this.props.history.push({pathname: '/routerLife/TestRouter'})
   }
 
   render() {
