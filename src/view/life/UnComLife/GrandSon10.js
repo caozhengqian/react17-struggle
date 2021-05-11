ae9da3fcb220bba1141b5feefa5ebf6f29ac25c1
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-05-11 19:51:50 +0800

    add 下班保存

--- src/view/life/UnComLife/GrandSon10.js
+++ src/view/life/UnComLife/GrandSon10.js
@@ -3,31 +3,31 @@ import "./index.less";
 
 export default class GrandSon10 extends React.Component {
   constructor(props) {
-    console.log(`%c            GrandSon10-->1、constructor`, "color:#600000");
+    console.log(`%c            GrandSon10-->1、constructor`, "color:#181d4b");
     super(props);
     this.state = {};
   }
 
   static getDerivedStateFromProps(props, state){
-    console.log(`%c            GrandSon10-->2、getDerivedStateFromProps`, "color:#600000");
+    console.log(`%c            GrandSon10-->2、getDerivedStateFromProps`, "color:#181d4b");
     return null;
   }
   componentDidMount() {
-    console.log(`%c            GrandSon10-->3、componentDidMount`, "color:#600000");
+    console.log(`%c            GrandSon10-->3、componentDidMount`, "color:#181d4b");
   }
   shouldComponentUpdate(){
-    console.log(`%c            GrandSon10-->4、shouldComponentUpdate`, "color:#600000");
+    console.log(`%c            GrandSon10-->4、shouldComponentUpdate`, "color:#181d4b");
     return true
   }
   getSnapshotBeforeUpdate(){
-    console.log(`%c            GrandSon10-->5、getSnapshotBeforeUpdate`, "color:#600000");
+    console.log(`%c            GrandSon10-->5、getSnapshotBeforeUpdate`, "color:#181d4b");
     return null;
   }
   componentDidUpdate(){
-    console.log(`%c            GrandSon10-->6、componentDidUpdate`, "color:#600000");
+    console.log(`%c            GrandSon10-->6、componentDidUpdate`, "color:#181d4b");
   }
   componentWillUnmount(){
-    console.log(`%c            GrandSon10-->7、componentWillUnmount`, "color:#600000");
+    console.log(`%c            GrandSon10-->7、componentWillUnmount`, "color:#181d4b");
   }
   render() {
     return (
--- src/view/life/UnComLife/Son10.js
+++ src/view/life/UnComLife/Son10.js
@@ -4,31 +4,31 @@ import GrandSon10 from './GrandSon10'
 
 export default class Son10 extends React.Component {
   constructor(props) {
-    console.log(`%c       Son10-->1、constructor`, "color:#CE0000");
+    console.log(`%c       Son10-->1、constructor`, "color:#4e72b8");
     super(props);
     this.state = {};
   }
 
   static getDerivedStateFromProps(props, state){
-    console.log(`%c       Son10-->2、getDerivedStateFromProps`, "color:#CE0000");
+    console.log(`%c       Son10-->2、getDerivedStateFromProps`, "color:#4e72b8");
     return null;
   }
   componentDidMount() {
-    console.log(`%c       Son10-->3、componentDidMount`, "color:#CE0000");
+    console.log(`%c       Son10-->3、componentDidMount`, "color:#4e72b8");
   }
   shouldComponentUpdate(){
-    console.log(`%c       Son10-->4、shouldComponentUpdate`, "color:#CE0000");
+    console.log(`%c       Son10-->4、shouldComponentUpdate`, "color:#4e72b8");
     return true
   }
   getSnapshotBeforeUpdate(){
-    console.log(`%c       Son10-->5、getSnapshotBeforeUpdate`, "color:#CE0000");
+    console.log(`%c       Son10-->5、getSnapshotBeforeUpdate`, "color:#4e72b8");
     return null;
   }
   componentDidUpdate(){
-    console.log(`%c       Son10-->6、componentDidUpdate`, "color:#CE0000");
+    console.log(`%c       Son10-->6、componentDidUpdate`, "color:#4e72b8");
   }
   componentWillUnmount(){
-    console.log(`%c       Son10-->7、componentWillUnmount`, "color:#CE0000");
+    console.log(`%c       Son10-->7、componentWillUnmount`, "color:#4e72b8");
   }
   render() {
     return (
