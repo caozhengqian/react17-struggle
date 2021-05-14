f92159909bead6a2eadd0e602de23546c9ea2d74
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-05-14 15:35:51 +0800

    add 循环结束

--- src/view/life/ForLife/ForLife.js
+++ src/view/life/ForLife/ForLife.js
@@ -4,15 +4,34 @@ import Son from "./Son";
 
 
 export default class ForLife extends React.Component {
+
   constructor(props) {
+    console.log(`%c Father-->1、constructor`, "color:#FF9797");
     super(props);
     this.state = {
       list:['a','b']
     };
   }
-
+  static getDerivedStateFromProps(props, state){
+    console.log(`%c Father-->1、getDerivedStateFromProps`, "color:#FF9797");
+    return null;
+  }
   componentDidMount() {
-
+    console.log(`%c Father-->1、componentDidMount`, "color:#FF9797");
+  }
+  shouldComponentUpdate(){
+    console.log(`%c Father-->1、shouldComponentUpdate`, "color:#FF9797");
+    return true
+  }
+  getSnapshotBeforeUpdate(){
+    console.log(`%c Father-->1、getSnapshotBeforeUpdate`, "color:#FF9797");
+    return null;
+  }
+  componentDidUpdate(){
+    console.log(`%c Father-->1、componentDidUpdate`, "color:#FF9797");
+  }
+  componentWillUnmount(){
+    console.log(`%c Father-->1、componentWillUnmount`, "color:#FF9797");
   }
   handleClick(){
     if (JSON.stringify(this.state.list) === JSON.stringify(['a', 'b'])) {
