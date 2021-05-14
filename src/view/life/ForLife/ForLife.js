b750b6a0e3d4c539d8bb067a0a70ad679de24c09
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-05-14 08:57:22 +0800

    add 。。。

--- src/view/life/ForLife/ForLife.js
+++ src/view/life/ForLife/ForLife.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import "./index.less";
+import Son from "./Son";
+
 
 export default class ForLife extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      list:['a','b']
+    };
   }
 
   componentDidMount() {
 
   }
-
+  handleClick(){
+    if (JSON.stringify(this.state.list) === JSON.stringify(['a', 'b'])) {
+      this.setState({list:['b', 'c']})
+    } else {
+      this.setState({list:['a', 'b']})
+    }
+  }
   render() {
     return (
         <div className="forLife">
           ForLife
+          <button  onClick={() => this.handleClick()}>切换组件</button>
+          {
+            this.state.list.map((value,key)=>{
+              return(
+                  <Son value={value} key={key}/>
+                  )
+
+            })
+          }
+
         </div>
     );
   }
